95fcdb4ff49b031720f6d56d42bf760f71b742bb
rings-of-saturn <rings-of-saturn>
2026-07-26 19:09:26 +0300

    level 10

--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -8,7 +8,7 @@ const CARD_BASE = preload("uid://8pd88rktly3w")
 const CARD_SPEED = preload("uid://byixd3vivdxyr")
 
 var current_first_card = CARD_BASE;
-var current_second_card = CARD_BASE;
+var current_second_card = CARD_SPEED;
 var current_third_card = CARD_BASE;	
 var enabled_cards : Array[int]
 
